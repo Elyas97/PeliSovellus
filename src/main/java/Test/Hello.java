980a8509b09f10d1e192bb449a51cfc89e2ce747
Jamppa <jamppaa@gmail.com>
2021-09-01 13:30:10 +0300

    Hello

---
+++ src/main/java/Test/Hello.java
@@ -0,0 +1,13 @@
+package Test;
+
+import java.util.Iterator;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Testi" + " x" +i);
+		}
+	}
+
+}
