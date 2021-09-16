7bd9574d1e8daab3b14d5ed188b02210e997a996
jasmija <jasmin.janka@metropolia.fi>
2021-09-16 20:37:02 +0300

    Käyttäjä luokalle testit ja LisääPeli luokassa enumin käytön testailua

--- src/main/java/model/Peli.java
+++ src/main/java/model/Peli.java
@@ -1,18 +1,26 @@
 package model;
 
+
 public class Peli {
 
 	private int peliId;
 	private String pelinNimi;
-	private String pelinTyyppi;
 	private int tietoId;
+	private Pelintyyppi tyyppi;
 
+	public enum Pelintyyppi {
+		DEFAULT, LAUTAPElI, VIDEOPELI
+	}
 	
-	public Peli(int peliId, String pelinNimi, String pelinTyyppi, int tietoId) {
+	public Peli(int peliId, String pelinNimi, Pelintyyppi tyyppi, int tietoId) {
 		this.peliId = peliId;
 		this.pelinNimi = pelinNimi;
-		this.pelinTyyppi = pelinTyyppi;
 		this.tietoId = tietoId;
+		this.tyyppi = tyyppi;
+	}
+	
+	public Peli() {
+		
 	}
 	
 	public int getPeliId() {
@@ -23,15 +31,23 @@ public class Peli {
 		this.peliId = peliId;
 	}
 	
+	public void setPelinNimi(String pelinNimi) {
+		this.pelinNimi = pelinNimi;
+	}
+	
 	public String getPelinNimi() {
 		return pelinNimi;
 	}
 	
-	public void setPelinTyyppi(String pelinTyyppi) {
-		this.pelinTyyppi = pelinTyyppi;
+	public void setPelinTyyppi(Pelintyyppi tyyppi) {
+		this.tyyppi = tyyppi;
 	}
 	
-	public int getTietoId(int tietoId) {
+	public Pelintyyppi getPelintyyppi() {
+		return tyyppi;
+	}
+	
+	public int getTietoId() {
 		return tietoId;
 	}
 	
---
+++ src/test/java/model/KäyttäjäTest.java
@@ -0,0 +1,97 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class KäyttäjäTest {
+
+	/*@Test
+	void testKäyttäjäIntStringStringStringIntString() {
+	}*/
+
+	@Test
+	void testKäyttäjä() {
+		Käyttäjä käyttäjä = new Käyttäjä(1, "salasana", "Niko", "Nieminen", 408322313, "moivaan@gmail.com");
+		assertEquals(1, käyttäjä.getKayttajaID(), "Käyttäjän id on väärin.");
+	}
+
+	@Test
+	void testGetKayttajaID() {
+		Käyttäjä käyttäjä = new Käyttäjä(1, "salasana", "Niko", "Nieminen", 408322313, "moivaan@gmail.com");
+		assertEquals(1, käyttäjä.getKayttajaID(), "Käyttäjän id väärin.");
+	}
+
+	@Test
+	void testSetKayttajaID() {
+		Käyttäjä käyttäjä = new Käyttäjä();
+		käyttäjä.setKayttajaID(1);
+        assertTrue(käyttäjä.getKayttajaID() == 1);
+	}
+
+	@Test
+	void testGetSalasana() {
+		Käyttäjä käyttäjä = new Käyttäjä(1, "salasana", "Niko", "Nieminen", 408322313, "moivaan@gmail.com");
+		assertEquals("salasana", käyttäjä.getSalasana(), "Käyttäjän salasana väärin.");
+	}
+
+	@Test
+	void testSetSalasana() {
+		Käyttäjä käyttäjä = new Käyttäjä();
+		käyttäjä.setSalasana("1234");
+        assertTrue(käyttäjä.getSalasana() == "1234");
+	}
+
+	@Test
+	void testGetEtunimi() {
+		Käyttäjä käyttäjä = new Käyttäjä(1, "salasana", "Niko", "Nieminen", 408322313, "moivaan@gmail.com");
+		assertEquals("Niko", käyttäjä.getEtunimi(), "Käyttäjän etunimi väärin.");
+	}
+
+	@Test
+	void testSetEtunimi() {
+		Käyttäjä käyttäjä = new Käyttäjä();
+		käyttäjä.setEtunimi("Niko");
+        assertTrue(käyttäjä.getEtunimi() == "Niko");
+	}
+
+	@Test
+	void testGetSukunimi() {
+		Käyttäjä käyttäjä = new Käyttäjä(1, "salasana", "Niko", "Nieminen", 408322313, "moivaan@gmail.com");
+		assertEquals("Nieminen", käyttäjä.getSukunimi(), "Käyttäjän sukunimi väärin.");
+	}
+
+	@Test
+	void testSetSukunimi() {
+		Käyttäjä käyttäjä = new Käyttäjä();
+		käyttäjä.setSukunimi("Nieminen");
+        assertTrue(käyttäjä.getSukunimi() == "Nieminen");
+	}
+
+	@Test
+	void testGetPuhelinumero() {
+		Käyttäjä käyttäjä = new Käyttäjä(1, "salasana", "Niko", "Nieminen", 408322313, "moivaan@gmail.com");
+		assertEquals(408322313, käyttäjä.getPuhelinumero(), "Käyttäjän puhelinnumero väärin.");
+	}
+
+	@Test
+	void testSetPuhelinumero() {
+		Käyttäjä käyttäjä = new Käyttäjä();
+		käyttäjä.setPuhelinumero(045321454);
+        assertTrue(käyttäjä.getPuhelinumero() == 045321454);
+	}
+
+	@Test
+	void testGetSähköposti() {
+		Käyttäjä käyttäjä = new Käyttäjä(1, "salasana", "Niko", "Nieminen", 408322313, "moivaan@gmail.com");
+		assertEquals("moivaan@gmail.com", käyttäjä.getSähköposti(), "Käyttäjän sähköposti väärin.");
+	}
+
+	@Test
+	void testSetSähköposti() {
+		Käyttäjä käyttäjä = new Käyttäjä();
+		käyttäjä.setSähköposti("nikke@hotmail.com");
+        assertTrue(käyttäjä.getSähköposti() == "nikke@hotmail.com");
+	}
+}
