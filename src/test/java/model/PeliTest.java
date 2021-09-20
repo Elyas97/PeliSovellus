85faf7515c056a3d18b58f8b1563315196efb259
jasmija <jasmin.janka@metropolia.fi>
2021-09-20 09:34:55 +0300

    Tapahtumat.fxml ja Uusipeli.fxml pohjat

---
+++ src/test/java/model/PeliTest.java
@@ -0,0 +1,59 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PeliTest {
+
+	@Test
+	void testPeliIntStringPelintyyppiInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPeli() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPeliId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPeliId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPelinNimi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPelinNimi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPelinTyyppi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPelintyyppi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTietoId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTietoId() {
+		fail("Not yet implemented");
+	}
+
+}
