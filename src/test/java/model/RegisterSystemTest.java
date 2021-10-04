ea4adee31af584303dc06fa2fc252aca04962efc
Alex Kekkonen <eikei@LAPTOP-NND5Q1GM>
2021-10-04 09:40:19 +0300

    "rekisteri testi pohja"

---
+++ src/test/java/model/RegisterSystemTest.java
@@ -0,0 +1,47 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class RegisterSystemTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	final void testRegisterSystem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testRegister() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	
+	
+	
+	@Test
+	final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
